11bd627612093535540852bfcdcf5a6d1d09481b
Matt Reider <mreider@gmail.com>
2019-06-26 22:24:12 -0700

    password page

---
+++ README.md
@@ -0,0 +1,6 @@
+Required environment variables
+
+export FLASK_APP=main.py
+export FLASK_DEBUG=1
+export RORO_SECRET_KEY=foo
+export RORO_PASSWORD=foo
--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from flask import Flask
 from flask import render_template
 from flask import session, redirect, url_for, escape, request
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from flask_debugtoolbar import DebugToolbarExtension
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 app = Flask(__name__)
-app.secret_key = 'foo0bar0buzz'.encode('utf8')
-
+app.secret_key = os.environ['RORO_SECRET_KEY'].encode('utf8')
+toolbar = DebugToolbarExtension(app)
 @app.route('/',methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',session=session)
+    error = None
+    if request.method == 'POST':
+        if request.form['password'].lower() == os.environ['RORO_PASSWORD']:
+            session['authorized'] = True
+            return render_template('index.html', session=session, error=error)
+        else:
+            error = 'Invalid password'
+    return render_template('index.html', session=session, error=error)
